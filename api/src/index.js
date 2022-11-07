3a366bbbe9f56190f7686d7ce9cc26355ddf7349
ThomRoman <thomtwd@gmail.com>
2022-11-06 23:46:56 -0500

    update

--- api/src/index.js
+++ api/src/index.js
@@ -7,7 +7,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT_BACKEND || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(bodyParserErrorHandler());
