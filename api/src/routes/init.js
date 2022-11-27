54308b6a0941e8ffda23a5bfd6d5ab4ab5839b31
ThomRoman <thomtwd@gmail.com>
2022-11-27 18:31:10 -0500

    update backend

--- api/src/routes/init.js
+++ api/src/routes/init.js
@@ -1,15 +1,30 @@
-import sequelize from "./../libs/sequelize.js";
+import sequelize from "../libs/sequelize.js";
 import fs from "fs";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 import readline from "readline";
+import { DataTypes } from "sequelize";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const PATH_FILE = join(__dirname, "../..", "AgenRet_TXT.txt");
 
+const createTables = async () => {
+  const queryInterface = sequelize.getQueryInterface();
+  await queryInterface.createTable("exoneracion_retraccion", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    razon: DataTypes.STRING,
+    ruc: DataTypes.STRING,
+    a_partir_del: DataTypes.STRING,
+    resolucion: DataTypes.STRING,
+  });
+};
+
 const insertAgenteRetencion = async () => {
-  await sequelize.authenticate();
   const file = readline.createInterface(fs.createReadStream(PATH_FILE));
   const agentes = [];
   file.on("line", (line) => {
@@ -28,4 +43,11 @@ const insertAgenteRetencion = async () => {
   });
 };
 
-insertAgenteRetencion();
+const init = async () => {
+  await sequelize.drop();
+  await sequelize.authenticate();
+  await createTables();
+  await insertAgenteRetencion();
+};
+
+init();
